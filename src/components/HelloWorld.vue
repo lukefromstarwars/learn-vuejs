e5c44370f6067741f9da2a7897dbdc9a46f5c017
Frietje <lukefromstarwars@gmail.com>
2020-03-19 10:13:48 +0100

    components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,23 +6,49 @@
     <h1 @click="attachRed = !attachRed" :class="{ red: attachRed }">
       {{ sayHello() }}
     </h1>
-    <p>{{ counter }}</p>
+    <p v-if="attachRed">show red</p>
+    <p v-else>not red</p>
+    <v-btn
+      color="success"
+      @click="
+        {
+          counter++;
+        }
+      "
+      >{{ counter }}</v-btn
+    >
     <v-btn outline color="primary" dark>text</v-btn>
     <input v-model="color" type="text" />
+    <ul>
+      <li v-for="(person, i) in persons" :key="person.id">
+        {{ person }} {{ i }}
+      </li>
+    </ul>
+    <ul>
+      <li v-for="i in 10" v-bind:key="i">
+        <p>{{ i }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  data: () => ({
-    title: "hello the world!!! as title",
-    other: "other hello 22",
-    attachRed: false,
-    color: "gray"
-  }),
+  data() {
+    return {
+      title: "hello the world!!! as title",
+      other: "other hello 22",
+      attachRed: false,
+      color: "gray",
+      counter: 0,
+      persons: ["Pierre", "Fréd", "Luc", "Barbara"]
+    };
+  },
   methods: {
-    sayHello: () => "Hello as a function"
+    sayHello() {
+      return "Hello as a function 8";
+    }
   },
   props: {
     msg: String
